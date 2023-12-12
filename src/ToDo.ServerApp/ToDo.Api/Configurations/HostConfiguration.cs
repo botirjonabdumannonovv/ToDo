d1d9aa80f8f423fd3f5d1c6c6f5d25e2edc65fef
Botirjon <thedavlativich@gmail.com>
2023-12-12 12:44:42 +0500

    add code refactor

---
+++ src/ToDo.ServerApp/ToDo.Api/Configurations/HostConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace ToDo.Api.Configurations;
+
+public static partial class HostConfiguration
+{
+    public static ValueTask<WebApplicationBuilder> ConfigureAsync(this WebApplicationBuilder builder)
+    {
+        builder
+            .AddMappers()
+            .AddValidators()
+            .AddBusinessLogicInfrastructure()
+            .AddCors()
+            .AddExposers()
+            .AddDevTools();
+
+        return new(builder);
+    }
+
+    public static ValueTask<WebApplication> ConfigureAsync(this WebApplication app)
+    {
+        app.UseCors();
+        app.UseExposers().UseDevTools();
+
+        return new(app);
+    }
+}
--- src/ToDo.ServerApp/ToDo.Api/obj/Debug/net8.0/ToDo.Api.AssemblyInfo.cs
+++ src/ToDo.ServerApp/ToDo.Api/obj/Debug/net8.0/ToDo.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDo.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+593e37423d1d9bc2ac505f4e52dc69d7d02677c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDo.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDo.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
